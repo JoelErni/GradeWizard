6f8d3a134f4dd9b0d6693f0268128b42d60f1626
Joel Erni <joel.erni@hispeed.ch>
2023-05-04 11:30:21 +0200

    asd

--- index.php
+++ index.php
@@ -9,29 +9,10 @@
     <body>
         <div style="display: flex;">
             <div style="flex: 1">
-                <header>
-                    <div class="container text-center text-light">
-                        <div class="menu">
-                            <div>
-                                <h2>Startseite</h2>
-                            </div>
-                            <div>
-                                <h2>Noten</h2>
-                            </div>
-                            <div>
-                                <h2>Klasse</h2>
-                            </div>
-                            <div>
-                                <h2>Einstellungen</h2>
-                            </div>
-                            <div>
-                                <h2>Hilfe</h2>
-                            </div>
-                        </div>
-                    </div>
-                </header>
+                <!-- HEADER -->
+                <?php include("components\header.html")?>
             </div>
-            <div style="flex: 9" class="article-background">
+            <div style="flex: 7" class="article-background">
                 <article>
                     <div class="titel"> 
                         <h1 class="text-center">GRADE<span><img alt="Logo" src="/src/images/logo.jpg" class="logo"></span>WIZARD</h1>
@@ -122,6 +103,8 @@
                 </article>        
             </div>
         </div>
+        <!-- FOOTER -->
+        <?php include("components/footer.html")?>
     </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </html>
---
+++ noten.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <title>Noten</title>
+        <meta charset="utf-8" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <div style="display: flex;">
+            <div style="flex: 1">
+                <!-- HEADER -->
+                <?php include("components\header.html")?>
+            </div>
+            <div style="flex: 7" class="article-background">
+                <article>
+                    
+                </article>        
+            </div>
+        </div>
+        <!-- FOOTER -->
+        <?php include("components/footer.html")?>
+    </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</html>
