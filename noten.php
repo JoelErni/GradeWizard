bfcea591d668f586e15870628c72ac9a2603b07e
JeppyXD <102746806+JeppyXD@users.noreply.github.com>
2023-05-18 14:08:22 +0200

    css header footer fix

--- noten.php
+++ noten.php
@@ -95,11 +95,11 @@
                             }
                         ?>
                     </table>
-                </article>        
+                </article>   
+                <!-- FOOTER -->
+                <?php include("components/footer.html"); ?>
             </div>
         </div>
-        <!-- FOOTER -->
-        <?php include("components/footer.html"); ?>
     </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </html>
